3bae7f6b52ad12f532eec1940f91f0ac99acad8e
Enrico <kese87@gmail.com>
2023-01-27 22:25:09 +0100

    hostname

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class Controller {
 
     @GetMapping("/ping")
-    public String ping(){
-        return "pong";
+    public String ping() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostName() + ": pong";
     }
 
 }
