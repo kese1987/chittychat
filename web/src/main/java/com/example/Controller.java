371dfa77d3b6edb51a56203e36b589170b388816
Enrico <kese87@gmail.com>
2023-01-29 00:50:49 +0100

    seems better

--- web/src/main/java/com/example/Controller.java
+++ web/src/main/java/com/example/Controller.java
@@ -2,6 +2,8 @@ package com.example;
 
 import com.example.commander.sender.Sender;
 import com.example.commands.Ping;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,28 +16,38 @@ public class Controller {
 
     private final Sender sender;
     private CBORSerializer serializer;
+    private ObjectMapper mapper;
 
     public Controller(Sender sender,
-                      CBORSerializer serializer) {
+                      CBORSerializer serializer,
+                      ObjectMapper mapper) {
         this.sender = sender;
         this.serializer = serializer;
+        this.mapper = mapper;
     }
 
-    @GetMapping("ping")
-    public CompletableFuture<String> ping() throws UnknownHostException {
+    @GetMapping(value = "ping", produces = {"application/json"})
+    public CompletableFuture<String> ping() {
 
         byte[] ping = serializer.serialize(new Ping("Hello neighbor, I'm your master!"));
 
         return sender.send(ping)
-                .thenApply(results -> results.stream().map(it -> {
-                    String msg = it.pod().getName() + "[" + it.pod().getAddress() + "] said: ";
-                    List<String> strings = it.results().stream()
-                            .map(result -> serializer.deserialize(result.result(), Ping.Result.class))
-                            .map(Ping.Result::response).toList();
+                .thenApply(results -> {
+                    List<String> replies = results.stream().map(it -> {
+                        String msg = it.pod().getName() + "[" + it.pod().getAddress() + "] said: ";
+                        List<String> strings = it.results().stream()
+                                .map(result -> serializer.deserialize(result.result(), Ping.Result.class))
+                                .map(Ping.Result::response).toList();
 
-                    return msg + strings;
+                        return msg + strings;
 
-                }).toList().toString())
+                    }).toList();
+                    try {
+                        return mapper.writeValueAsString(replies);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
                 .exceptionally(Throwable::getLocalizedMessage);
 
 
